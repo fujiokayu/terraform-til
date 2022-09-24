6bc779516f74b2c5ebb85d9b81ba148795d2e0ec
yu fujioka <fujiokayu@gmail.com>
2022-09-24 23:22:22 +0900

    add application load balancer

--- main.tf
+++ main.tf
@@ -112,10 +112,70 @@ resource "aws_route" "private_1" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-module "example_sg" {
+module "http_sg" {
   source      = "./security_group"
-  name        = "module-sg"
+  name        = "http-sg"
   vpc_id      = aws_vpc.example.id
   port        = 80
   cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source      = "./security_group"
+  name        = "https-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_redirect_sg" {
+  source      = "./security_group"
+  name        = "http-redirect-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 8080
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform"
+}
+
+resource "aws_lb" "example" {
+  name                       = "example"
+  load_balancer_type         = "application"
+  internal                   = false
+  idle_timeout               = 60
+  enable_deletion_protection = true
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id,
+  ]
+
+  access_logs {
+    bucket  = aws_s3_bucket.alb_log.id
+    enabled = true
+  }
+
+  security_groups = [
+    module.http_sg.security_group_id,
+    module.https_sg.security_group_id,
+    module.http_redirect_sg.security_group_id,
+  ]
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "alb_log_lifecycle_configuration" {
+  bucket  = aws_s3_bucket.alb_log.id
+  rule {
+    # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_lifecycle_configuration
+    id = "rule-1"
+    status = "Enabled"
+    expiration {
+      days = "180"
+    }
+  }
 }
